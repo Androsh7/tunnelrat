522974cad83c97711360942fc8e616a7a35350b7
Androsh7 <150483650+Androsh7@users.noreply.github.com>
2026-09-19 15:36:28 -0600

    Bugfix/fix build issues (#2)
    * Added registry changes (add exe to path) to the installer script
    
    * publish workflow fires independent of tag workflow
    
    * Fixed issues where installer would not include script doc
    
    * Updated version and changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,14 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.1.1]
+
+### Fixed
+
+- PyPI publish would fail (it now fires independent of the tag step)
+- Compiled executables now are able to reference the example script
+- Windows installer now adds tunnelrat to the path
+
 ## [0.1.0]
 
 ### Added
--- compile_script.py
+++ compile_script.py
@@ -19,12 +19,6 @@ DEFAULT_OUTPUT_DIRECTORY = REPOSITORY_ROOT / "dist"
 PROJECT_NAME = "tunnelrat"
 WINDOWS_EXECUTABLE_SUFFIX = ".exe"
 
-# Data the compiled package reads at runtime, mapped as source=destination inside the payload
-INCLUDED_DATA_FILES = (
-    "VERSION.txt=VERSION.txt",
-    "tunnelrat/docs/example_script.yaml=docs/example_script.yaml",
-)
-
 GLIBC_LIBRARY_NAME = "glibc"
 
 # The folder and executable Nuitka produces for a standalone build of the entry point
@@ -208,7 +202,7 @@ class BuildConfiguration:
             command.append(f"--windows-file-version={read_version()}")
         if self.assume_yes_for_downloads:
             command.append("--assume-yes-for-downloads")
-        command.extend(f"--include-data-file={data_file}" for data_file in INCLUDED_DATA_FILES)
+        command.append(f"--include-package-data={PROJECT_NAME}")
         command.append(str(ENTRY_POINT))
         return command
 
@@ -236,6 +230,11 @@ def build_installer_script(configuration: BuildConfiguration) -> str:
             f"ArchitecturesInstallIn64BitMode={architecture_identifier}",
             "Compression=lzma2",
             "SolidCompression=yes",
+            "ChangesEnvironment=yes",
+            "",
+            "[Tasks]",
+            'Name: "addtopath"; Description: "Add tunnelrat to the PATH environment variable"; '
+            'GroupDescription: "Environment:"',
             "",
             "[Files]",
             f'Source: "{configuration.distribution_directory}\\*"; DestDir: "{{app}}"; '
@@ -243,6 +242,25 @@ def build_installer_script(configuration: BuildConfiguration) -> str:
             "",
             "[Icons]",
             f'Name: "{{group}}\\{PROJECT_NAME}"; Filename: "{{app}}\\{STANDALONE_EXECUTABLE_NAME}"',
+            "",
+            "[Registry]",
+            'Root: HKLM; Subkey: "SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment"; '
+            'ValueType: expandsz; ValueName: "Path"; ValueData: "{olddata};{app}"; '
+            "Tasks: addtopath; Check: NeedsAddPath('{app}')",
+            "",
+            "[Code]",
+            "function NeedsAddPath(Param: string): Boolean;",
+            "var",
+            "  ExistingPath: string;",
+            "begin",
+            "  if not RegQueryStringValue(HKEY_LOCAL_MACHINE, "
+            "'SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment', 'Path', ExistingPath) then",
+            "  begin",
+            "    Result := True;",
+            "    exit;",
+            "  end;",
+            "  Result := Pos(';' + Uppercase(ExpandConstant(Param)) + ';', ';' + Uppercase(ExistingPath) + ';') = 0;",
+            "end;",
             "",
         ]
     )
